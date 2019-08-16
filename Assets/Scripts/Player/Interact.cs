e3a725633fed16a04b7c2c06c6c0e0b4be40ea02
JamesNemeth <jamesnemeth1999@gmail.com>
2019-08-16 11:58:29 +1000

    Initial Upload
    TAFE RPG game, first upload

---
+++ Assets/Scripts/Player/Interact.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact"))
+        {
+            Ray interactionRay;
+            interactionRay = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+            RaycastHit hitInfo;
+            if(Physics.Raycast(interactionRay, out hitInfo, 10))
+            {
+                switch(hitInfo.collider.tag)
+                {
+                    case "NPC":
+                        Debug.Log("NPC Interaction");
+                    break;
+                    case "Item":
+                        Debug.Log("Picked up Item");
+                    break;
+
+                }
+                /*
+                if (hitInfo.collider.CompareTag("NPC"))
+                {
+                    Debug.Log("NPC Interaction");
+                }
+                if (hitInfo.collider.CompareTag("Item"))
+                {
+                    Debug.Log("Pick up Item");
+                }
+                */
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHandler : MonoBehaviour
+{
+    [Header("Value Variables")]
+
+    public float maxHealth;
+    public float curHealth, curMana, curStamina, maxMana, maxStamina;
+    [Header("Value Variables")]
+
+    public Slider healthBar;
+    public Slider manaBar;
+    public Slider staminaBar;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(healthBar.value != Mathf.Clamp01(curHealth / maxHealth))
+        {
+            curHealth = Mathf.Clamp(curHealth, 0, maxHealth);
+            healthBar.value = Mathf.Clamp01(curHealth / maxHealth);
+        }
+        if (manaBar.value != Mathf.Clamp01(curMana / maxMana))
+        {
+            curMana = Mathf.Clamp(curMana, 0, maxHealth);
+            manaBar.value = Mathf.Clamp01(curMana / maxMana);
+        }
+        if (staminaBar.value != Mathf.Clamp01(curStamina / maxStamina)) 
+        {
+            curStamina = Mathf.Clamp(curStamina, 0, maxStamina);
+            staminaBar.value = Mathf.Clamp01(curStamina / maxStamina);
+        }
+    }
+}
